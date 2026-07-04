fdc6b9469e2bd84512a506944febabe09ef24682
Jganyo-dotcom <elikemejay@gmail.com>
2026-07-04 01:24:53 +0000

    added newbeies

--- src/modules/user_model/User_controller.js
+++ src/modules/user_model/User_controller.js
@@ -136,7 +136,7 @@ const LoginUser = async (req, res) => {
     }
 
     // OTP verification flow for non-managers
-    if (tryingToLoginUser.isVerified) {
+    if (!tryingToLoginUser.isVerified) {
       try {
        
 
